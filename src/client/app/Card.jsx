2855ee3f96767045dee468b332689445d6b3dd86
rufasa85 <rufasa85@gmail.com>
2018-03-14 16:29:49 -0700

    color change on click

--- src/client/app/Card.jsx
+++ src/client/app/Card.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 export class Card extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {class:'card ', color:'red'};
+		this.changeColor = this.changeColor.bind(this);
+	}
+
+	changeColor() {
+		this.setState({class: 'card ' + this.state.color + '-card '});
+		console.log(this.state.class);
+	}
 
 	render() {
 		return (
-			<div className='card'>
+			<div className= {this.state.class} onClick= {this.changeColor}>
 				<p>{this.props.word}</p>
 			</div>
 		)
